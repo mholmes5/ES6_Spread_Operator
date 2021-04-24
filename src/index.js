8e335383ca59d488bbe1cd3360a8c4448be2846c
Michael Holmes <michaelholmes618@gmail.com>
2021-04-24 13:59:18 -0500

    spread operator examples

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["Lime", "Watermelon", "Mango", "Tangerine"];
+const fruits = ["Apple", "Banana", "Strawberry", "Grape", ...citrus];
+
+console.log(fruits);
+
+const fullName = {
+  fName: "Sly",
+  lName: "Holmes"
+};
+
+const user = {
+  ...fullName,
+  email: "sly@nowalks.com"
+};
+
+console.log(user);
